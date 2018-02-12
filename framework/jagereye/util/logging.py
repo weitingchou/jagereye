f803331bdbc9e27563e9040874df4c781ccba1ad
uniray7 <uniray7@gmail.com>
2018-02-12 18:00:22 +0800

    refactor logging to class and configurable and apply new logger to brain

--- framework/jagereye/util/logging.py
+++ framework/jagereye/util/logging.py
@@ -4,86 +4,82 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import sys
 import logging as _logging
+import logging.config
 from logging import DEBUG # pylint: disable=unused-import
 from logging import ERROR # pylint: disable=unused-import
 from logging import FATAL # pylint: disable=unused-import
 from logging import INFO # pylint: disable=unused-import
 from logging import WARN # pylint: disable=unused-import
 
-from jagereye.util import static_util
 from jagereye.util.generic import get_config
 
-CONFIG = get_config()['logging']
+config = get_config()['logging']
+_logging.config.dictConfig(config)
 
-LEVEL = CONFIG['level'].upper()
+class Logger(object):
+    def __init__(self, component):
+        self.component = component
+        self.extra_info = {'component': self.component}
+        self._logger = _logging.getLogger('default')
 
-_logging.basicConfig(level=LEVEL)
-_logger = _logging.getLogger('jagereye')
+    def log(self, level, msg, *args, **kwargs):
+        """Log message for a given level.
 
+        Args:
+          level (int): The log level.
+          msg (string): The message to log.
+        """
+        self._logger.log(level, msg, extra=self.extra_info, *args, **kwargs)
 
-def log(level, msg, *args, **kwargs):
-    """Log message for a given level.
+    def debug(self, msg, *args, **kwargs):
+        """Log debug level message.
 
-    Args:
-      level (int): The log level.
-      msg (string): The message to log.
-    """
-    _logger.log(level, msg, *args, **kwargs)
+        Args:
+          msg (string): The message to log.
+        """
+        self._logger.debug(msg, extra=self.extra_info, *args, **kwargs)
 
+    def error(self, msg, *args, **kwargs):
+        """Log error level message.
 
-def debug(msg, *args, **kwargs):
-    """Log debug level message.
+        Args:
+          msg (string): The message to log.
+        """
+        self._logger.error(msg, extra=self.extra_info, *args, **kwargs)
 
-    Args:
-      msg (string): The message to log.
-    """
-    _logger.debug(msg, *args, **kwargs)
+    def fatal(self, msg, *args, **kwargs):
+        """Log fatal level message.
 
+        Args:
+          msg (string): The message to log.
+        """
+        self._logger.fatal(msg, extra=self.extra_info, *args, **kwargs)
 
-def error(msg, *args, **kwargs):
-    """Log error level message.
+    def info(self, msg, *args, **kwargs):
+        """Log info level message.
 
-    Args:
-      msg (string): The message to log.
-    """
-    _logger.error(msg, *args, **kwargs)
+        Args:
+          msg (string): The message to log.
+        """
+        self._logger.info(msg, extra=self.extra_info, *args, **kwargs)
 
+    def warn(self, msg, *args, **kwargs):
+        """Log warn level message.
 
-def fatal(msg, *args, **kwargs):
-    """Log fatal level message.
+        Args:
+          msg (string): The message to log.
+        """
+        self._logger.warn(msg, extra=self.extra_info, *args, **kwargs)
 
-    Args:
-      msg (string): The message to log.
-    """
-    _logger.fatal(msg, *args, **kwargs)
+    def warning(self, msg, *args, **kwargs):
+        """Log warn level message.
 
-
-def info(msg, *args, **kwargs):
-    """Log info level message.
-
-    Args:
-      msg (string): The message to log.
-    """
-    _logger.info(msg, *args, **kwargs)
-
-
-def warn(msg, *args, **kwargs):
-    """Log warn level message.
-
-    Args:
-      msg (string): The message to log.
-    """
-    _logger.warn(msg, *args, **kwargs)
-
-
-def warning(msg, *args, **kwargs):
-    """Log warn level message.
-
-    Args:
-      msg (string): The message to log.
-    """
-    _logger.warning(msg, *args, **kwargs)
+        Args:
+          msg (string): The message to log.
+        """
+        self._logger.warning(msg, extra=self.extra_info, *args, **kwargs)
 
 
 # Controls which methods from pyglib.logging are available within the project.
